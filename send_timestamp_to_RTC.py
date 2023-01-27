0df9d6ce36e2479ede860a69ec562d17e3e94fc7
Pascal Pieper <accounts@pascalpieper.de>
2023-01-27 18:49:16 +0100

    Enabled mkr use by moving to different RTC library

--- send_timestamp_to_RTC.py
+++ send_timestamp_to_RTC.py
@@ -2,30 +2,47 @@
 
 import serial
 from serial.tools import list_ports
+import datetime
+from datetime import timezone
 import time
+
+seconds_from_1970_to_2000 = datetime.datetime(2000,1,1,0,0,0,tzinfo=timezone.utc).timestamp()
 
 while(True):
     print("Available COM-Ports:")
     print([comport.device for comport in serial.tools.list_ports.comports()])
-    port = input("Choose a port: ")
+    if(len(serial.tools.list_ports.comports()) == 0):
+        time.sleep(.5)
+        break
+    port = serial.tools.list_ports.comports()[0].device
+
+    if(len(serial.tools.list_ports.comports()) > 1):
+        port = input("Choose a port: ")
     try:
-        s = serial.Serial(port, baudrate=115200)
+        s = serial.Serial(port, baudrate=115200,timeout=.5)
     except (OSError, serial.SerialException):
         print("Could not open port: " + str(OSError))
+        time.sleep(.5)
         continue
-    print("Success. Sending timestamp... Exit with ctrl-c")
+
+    print("Sending timestamp... Exit with ctrl-c")
 
     try:
         while(s.is_open):
-            print("Writing string " + str(int(time.time())))
-            s.write(str(str(int(time.time())) + '\n').encode('ascii'))
+            unix_timestamp = datetime.datetime.now(tz=timezone.utc).timestamp()
+            seconds_since_2000 = unix_timestamp - seconds_from_1970_to_2000
+            timestamp_to_send = (str(int(seconds_since_2000)) + '\r\n')
+            print("Writing string " + timestamp_to_send)
+            s.write(timestamp_to_send.encode('ascii'))
             s.flush()
-            retcode = s.read_until().decode('ascii')
+            retcode = s.readline().decode('ascii')
             print(retcode)
+            time.sleep(1)
             if("Success" in retcode):
                 print("nice.")
+                time.sleep(2)
                 break
-            time.sleep(0.5)
+            time.sleep(.5)
     except KeyboardInterrupt:
         s.close()
         exit()
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <DS1307RTC.h>
-#include <TimeLib.h>
+#include <RtcDS1307.h>
+
+RtcDS1307<TwoWire> external_Rtc(Wire);
 
 static constexpr unsigned max_bufsize = 64;
+
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt)
+{
+    char datestring[20];
+
+    snprintf_P(datestring,
+            countof(datestring),
+            PSTR("%02u.%02u.%04u %02u:%02u:%02u"),
+            dt.Day(),
+            dt.Month(),
+            dt.Year(),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second() );
+    Serial.print(datestring);
+}
 
 //necessary, because pow(x,y) returns float and produces rounding errors
 uint64_t powerOfTen(unsigned power) {
@@ -13,8 +32,8 @@ uint64_t powerOfTen(unsigned power) {
     return ret;
 }
 
-time_t charToTime(char* array){
-    time_t ret = 0;
+uint32_t charToTime(char* array){
+    uint32_t ret = 0;
     unsigned last = strnlen(array, max_bufsize);
     for(unsigned i = last; i > 0; i--){
         if(array[i-1] < '0' || array[i-1] > '9') {
@@ -30,30 +49,44 @@ time_t charToTime(char* array){
 }
 
 void setup() {
-  Serial.begin(115200);
+    external_Rtc.Begin();
+    Serial.begin(115200);
+    while (!Serial) {
+        digitalWrite(LED_BUILTIN, 1);
+        delay(250);
+        digitalWrite(LED_BUILTIN, 0);
+        delay(25);
+    }
 }
 
 void loop() {
     static char timestamp_c[max_bufsize+1];
+    memset(timestamp_c, 0, max_bufsize+1);
     uint8_t count = 0;
     while(count < max_bufsize) {
         while(!Serial.available()){};
         timestamp_c[count] = Serial.read();
-        if(timestamp_c[count] == '\r')
-            continue;
-        if(timestamp_c[count] == '\n') {
+        if(timestamp_c[count] == '\r' || timestamp_c[count] == '\n') {
             timestamp_c[count] = 0;
             break;
         }
+        // echo
+        Serial.print(timestamp_c[count]);
         count++;
     }
-    time_t new_timestamp = charToTime(timestamp_c);
-    if(new_timestamp < 1)
+    int32_t new_timestamp = charToTime(timestamp_c);
+    if(new_timestamp < 1) {
+        Serial.println("Skipping invalid timestamp");
         return;
+    }
 
-    if(RTC.set(new_timestamp)) {
+    // Remove stray chars from serial buffer
+    while(Serial.available()){ Serial.read(); }
+
+    external_Rtc.SetDateTime(RtcDateTime(new_timestamp));
+    if(external_Rtc.LastError() == 0) {
         Serial.print("Success: ");
-        Serial.println(RTC.get());
+        printDateTime(external_Rtc.GetDateTime());
     } else {
         Serial.println ("Failed to set RTC");
     }
